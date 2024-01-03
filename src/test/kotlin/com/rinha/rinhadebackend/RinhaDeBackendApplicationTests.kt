38aef88793cadd67b84d75decaff62443030a36c
Andre Rocha <andrerocha546@gmail.com>
2024-01-03 10:24:33 -0300

    first commit

---
+++ src/test/kotlin/com/rinha/rinhadebackend/RinhaDeBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rinha.rinhadebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RinhaDeBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
